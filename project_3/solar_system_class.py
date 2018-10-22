7ddd4003fec239eb7f18340849eac2566ee1c916
Marit Kollstuen <marikoll@student.geo.uio.no>
2018-10-22 18:33:53 +0200

    added dockstrings

--- project_3/solar_system_class.py
+++ project_3/solar_system_class.py
@@ -1,50 +1,78 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Oct 12 10:18:33 2018
+To run, use python master_document.py
 
-@author: maritkollstuen
+Project 3, task 3f,
+
+Module that calculates the trajectory of all planets defined in the system
+by using the velocity Verlet method and using the origin as center of mass
 """
 
 import numpy as np
-# import matplotlib.pyplot as plt
 import time
 
 
-class planets:   
+class planets:
+    """
+    Class that creates the planet-object
+    
+    Input: 
+        vel0    - <numpy array> initial velocity of the planet in AU/year
+        pos0    - <numpy array> initial position of the planet in AU
+        mass    - <float> mass of planet
+    """
     def __init__(self, vel0, pos0, mass):
-        self.vel0 = vel0        # AU/year
-        self.pos0 = pos0        # position in astronomical units
+        self.vel0 = vel0       
+        self.pos0 = pos0       
         self.mass = mass/2.10E30
         self.G = 4*np.pi**2
-        
-    def kinetic_energy(self, vel):
-        KE =  0.5*self.mass*np.linalg.norm(vel,axis=1)**2
-        return KE
-    
-    def potential_energy(self, r, mass_of_sun=1):
-        PE = -self.G*mass_of_sun*self.mass/np.linalg.norm(r, axis=1)#self.distance(pos)
-        return PE
-    
-    def angular_momentum(self, vel, r):
-        L = np.cross(r, vel*self.mass,axis=1)
-        return L
     
     def make_pos_vec(self, N):
+        """
+        Creates a position vector of size (N,3) with pos0 as first entry
+        
+        Input: 
+            N       - <int> length of vector
+        """        
         self.pos_vec = np.zeros((N,3))
         self.pos_vec[0] = self.pos0
 
     def make_vel_vec(self,N):
+        """
+        Creates a velocity vector of size (N,3) with vel0 as first entry
+        
+        Input: 
+            N       - <int> length of vector
+        """        
         self.vel_vec = np.zeros((N,3))
         self.vel_vec[0] = self.vel0
 
     def make_acc_vec(self,N,acc0):
+        """
+        Creates an acceleration vector of size (N,3) with acc0 as first entry
+        
+        Input: 
+            N       - <int> length of vector
+            acc0    - <numpy array> initial acceleration of the planet
+        """        
         self.acc_vec = np.zeros((N,3))
         self.acc_vec[0] = acc0
     
  
     
 def system(*args):
+    """
+    Function that generates a system of all input planets and adjusts the 
+    mass center to the origin
+    
+    Input: 
+        *args   - <class object of planets> planets to be used in further 
+                  calculations
+                  
+    Output: 
+        system  - <list> all planets in the system
+    """    
     system = []
     for i in args:
         system.append(i)
@@ -58,6 +86,15 @@ def system(*args):
     
     
 class solver: 
+    """
+    Class that calculates the trajectory of a planet by applying the velocity
+    Verlet method
+    
+    Input: 
+        system  - <list> all planets in the system
+        N       - <int> length of list
+        dt      - <float> step size
+    """    
     def __init__(self, system, N, dt):
         self.dt = dt
         self.N = N
@@ -71,6 +108,16 @@ class solver:
             i.make_acc_vec(N, self.acceleration(i, 0))
     
     def acceleration(self, current, i):
+        """
+        Calculates the acceleration of the planet by adjusting the 
+        gravitational force due to the presence of other planets
+        
+        Input: 
+            current - <class object of planets> current planet
+            i       - <int> index
+        Output: 
+            acc     - <numpy array> acceleration of planet
+        """        
         acc = np.zeros(3)
         
         for planet in self.system:
@@ -82,6 +129,10 @@ class solver:
 
         
     def velocity_verlet(self):
+        """
+        Calculates the trajectory of the planet by using the velocity Verlet
+        method
+        """        
         dt = self.dt
         dt05 = 0.5*dt
         dt2 = dt**2
