d9b16fcbf3b80acc6d4ae09c5c4a33bfa7f8c424
Johanne Mehren <johanmeh@student.matnat.uio.no>
2018-10-23 18:04:06 +0200

    cleared up

--- project_3/python_code/Mercury_Sun.py
+++ project_3/python_code/Mercury_Sun.py
@@ -27,14 +27,14 @@ def velocity_verlet(pos0, vel0, N, dt):
         precession - <numpy array> position of the trajectory
     """
     G = 4*np.pi**2
-    #c2 = 63239.7263**2
+    c2 = 63239.7263**2
     dt2 = dt**2
     
     start = time.time()
     
     pos = pos0
     vel = vel0
-    acc = -pos[0]*G/np.linalg.norm(pos)**3#*(1+(3*np.linalg.norm(np.cross(pos,vel))**2)/(np.linalg.norm(pos)**2*c2))
+    acc = -pos[0]*G/np.linalg.norm(pos)**3*(1+(3*np.linalg.norm(np.cross(pos,vel))**2)/(np.linalg.norm(pos)**2*c2))
 
     
     precession = []
@@ -45,7 +45,7 @@ def velocity_verlet(pos0, vel0, N, dt):
     l = np.linalg.norm(np.cross(posnew,vel))
     r = np.linalg.norm(posnew)
     
-    accnew = -posnew*G/r**3# *(1+(3*l**2)/(r**2*3999262982.498912))
+    accnew = -posnew*G/r**3*(1+(3*l**2)/(r**2*3999262982.498912))
     velnew = vel + 0.5*(accnew+accnew)*dt
     
     posprev = pos
@@ -62,7 +62,7 @@ def velocity_verlet(pos0, vel0, N, dt):
         l = np.linalg.norm(np.cross(posnew,vel))
         r = np.linalg.norm(posnew)
         
-        accnew = -posnew*G/r**3# *(1+(3*l**2)/(r**2*3999262982.498912))
+        accnew = -posnew*G/r**3*(1+(3*l**2)/(r**2*3999262982.498912))
         velnew = vel + 0.5*(accnew+accnew)*dt
         
         if np.linalg.norm(pos)< r and np.linalg.norm(pos)< np.linalg.norm(posprev):
