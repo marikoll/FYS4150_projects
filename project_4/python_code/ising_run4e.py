e1f4675506233744b9d3eed961542ed60ab17b3b
Marit Kollstuen <marikoll@student.geo.uio.no>
2018-11-16 16:01:33 +0100

    added cutoff

--- project_4/python_code/ising_run4e.py
+++ project_4/python_code/ising_run4e.py
@@ -10,34 +10,26 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 
-from Ising_model import MC
+from Ising_model import MC_cutoff
 
 
 spins       = [40, 60, 80, 100]
-trials      = int(1e7)
+trials      = int(1e6)
 
 temp = np.arange(2.0, 2.4, 0.01)
 
 sampled_energies = np.zeros((len(spins), len(temp)))
-sampled_magnets = np.zeros((len(spins), len(temp)))
 sampled_cv = np.zeros((len(spins), len(temp)))
 sampled_suscept = np.zeros((len(spins), len(temp)))
 sampled_absmagn = np.zeros((len(spins), len(temp)))
 
 start = time.time()
+p = 0.1 # take away 10 percent
 
-#fig1 = plt.figure(1)
-#fig2 = plt.figure(2)
-#fig3 = plt.figure(3)
-#fig4 = plt.figure(4)
 for k in range(len(temp)):
     for i in range(len(spins)):
         grid = np.ones((spins[i], spins[i]))
-        energy_avg, magnet_avg, C_v, susceptibility, abs_magnet, c = MC(grid, trials, temp[k])
-#        plt.plot(temp, energy_avg, label = 'L{}'.format(spins[i]))
-#        plt.plot(temp, abs_magnet, label = 'L{}'.format(spins[i]))
-#        plt.plot(temp, C_v, label = 'L{}'.format(spins[i]))
-#        plt.plot(temp, susceptibility, label = 'L{}'.format(spins[i]))
+        E, v, energy_avg, magnet_avg, C_v, susceptibility, abs_magnet = MC_cutoff(grid, trials, temp[k], p)
         sampled_energies[i, k] = energy_avg[-1]
         sampled_cv[i, k] = C_v
         sampled_suscept[i, k] = susceptibility
